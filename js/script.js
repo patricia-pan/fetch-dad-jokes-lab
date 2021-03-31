04ca501752400f6a5f1be0c2372dc360cf7d8cca
Your Name <you@example.com>
2021-03-31 00:41:07 -0700

    Update audio.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const myHeader = new Headers()
 // myHeaders.append('Content-Type', 'text/plain');
 // myHeaders.append('Content-Length', content.length.toString());
 // myHeaders.append('X-Custom-Header', 'ProcessThisImmediately');
-const magicPhrases = ["Don't spend it all in one place", "No, your other right", "Are you working hard, or hardly working?", "Money doesn't grow on trees", "Hi hungry, I'm Dad", "Who changed the thermostat?", "Don't tell your mother", "If you were told to jump off a cliff, would you?", "You know, when I was your age...", "You're not going out in that", "You'll understand when you're older", "If it ain't broke, don't fix it", "I'm not sleeping, I'm just resting my eyes"]
+// const magicPhrases = ["Don't spend it all in one place", "No, your other right", "Are you working hard, or hardly working?", "Money doesn't grow on trees", "Hi hungry, I'm Dad", "Who changed the thermostat?", "Don't tell your mother", "If you were told to jump off a cliff, would you?", "You know, when I was your age...", "You're not going out in that", "You'll understand when you're older", "If it ain't broke, don't fix it", "I'm not sleeping, I'm just resting my eyes"]
+const magicPhrases = ["Dear internet, please tell me how to be cool."]
 let index = 0
 
 function displayJoke(string){
